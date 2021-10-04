4359ccecb849aebcf4fe78302c7bbc9301c08088
Sydney <sydney@recroom.com>
2021-10-04 16:18:18 -0700

    Move nav to own file and remove example components

---
+++ src/components/nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from '@reach/router';
+
+export const Nav = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/blog">Blog</Link>
+    <Link to="/dynamic">Dynamic</Link>
+  </nav>
+)
